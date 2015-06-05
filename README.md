098403f485729b76de17445f107caac8fa8ec0ce
7sDream <didislover@gmail.com>
2015-06-05 18:37:49 +0800

    modify dir struct, fix openwifi problem with ubuntu 15.04

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 There are some useful files like .ini .mb .sh and others
 
 
-## QuickPhrase.mb
+## fcitx-quick-input/QuickPhrase.mb
 
 My phrase setting files in fcitx.
 
-## disable_keyboard.sh
+## kill_keyboad/disable_keyboard.sh
 
 It can disable your keyboard, when after run it, you will get another script to enable keyboard.
 
@@ -17,17 +17,17 @@ PS. Need xinput.
 sudo apt-get install xinput
 ```
 
-## open_wifi.sh
+## wifiAP/open_wifi.sh
 
 Open Wifi AP.
 
 Need wlan0 suppose ap mode.
 
-Need hostapd & dnsmasq.
+Need hostapd & dnsmasq & nmcli
 
 ```bash
 sudo apt-get install hostapd dnsmasq
 ```
 
 7sDream.
-2014-7-13
+2015-06-05
---
+++ wifiAP/open_wifi.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Useage : Open a Wifi AP;
+# author : 7sDream
+# Data : 2015.3.7
+
+# Need : hostapd & dnsmasq is installed , wlan0 suppose AP mode, doesn't connect to any AP now.
+
+# =============================
+# Turn down servces
+service dnsmasq stop
+service hostapd stop
+
+# setup network
+nmcli r wifi off
+rfkill unblock wlan
+killall hostapd
+killall dnsmasq
+
+# Open wlan0
+ifconfig wlan0 up
+
+# Set wlan0 ip address
+ifconfig wlan0 192.168.43.1
+
+# Configure NAT
+
+# Delete NAT rules
+iptables -t nat -F
+
+# Add NAT rule
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+# Enable ip forwoad
+sysctl net.ipv4.ip_forward=1
+
+
+# ========= Open WIFI ========
+
+echo -e "\n===== Open WiFi... =====\n"
+
+dnsmasq -C dnsmasq.conf
+hostapd hostapd.conf
+
+echo -e "\nWiFi Stop, Cleaning......\n"
+
+# ========= WIFI Stop ========
+
+# Stop services
+service dnsmasq stop
+service hostapd stop
+
+# Delete NAT Setting
+iptables -t nat -F
+
+# Disable ip forward
+sysctl net.ipv4.ip_forward=0
+nmcli r wifi on
+
+printf "\nDone!\n"
+
+read -n 1 -p "Press Enter key to exit..."
+
