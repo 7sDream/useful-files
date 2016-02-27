2cf684fb9ab7dc46bce6e4f41b97d8e202ee9f4d
7sDream <didislover@gmail.com>
2016-02-28 01:51:31 +0800

    update openwifi script

--- wifiAP/open_wifi.sh
+++ wifiAP/open_wifi.sh
@@ -1,15 +1,75 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Useage : Open a Wifi AP;
-# author : 7sDream
-# Data : 2015.3.7
+# Useage: Open a Wifi AP in debian based system;
+# author: 7sDream
 
-# Need : hostapd & dnsmasq & nmcli is installed , wlan0 suppose AP mode, doesn't connect to any AP now.
+# Depends :
+#   - hostapd & dnsmasq & nmcli is installed
+#   - A network card suppose AP mode, and doesn't connect to any AP now.
+
+# Changelog:
+#   - 2015.3.7:
+#       Fitst version, basic functional.
+#   - 2016.2.28:
+#       Chang SheBang.
+#       User can interactively change interface, ssid and password without edit conf files.
 
 # =============================
+
+# Make sure run as SU
 (( EUID != 0 )) && exec sudo -- "$0" "$@"
 
-BASEDIR=$(dirname $0)
+cd "$(dirname $0)"
+
+# Get network card names
+read -a interfaces <<< $(ip link show | sed -rn 's/^[0-9]+: (\w+):.*/\1/p')
+interfaces_length=${#interfaces[@]}
+
+# Print network card list
+for ((i = 0; i < ${interfaces_length} ; i++)); do
+    echo "$i: ${interfaces[$i]}"
+done;
+
+# ===== Get two network card name from user input =====
+function getIndex() {
+    # $1 for max length
+    # $2 for interfaces description
+
+    re_num=^[0-9]+$ # number regex
+    index=-1        # user input var
+
+    # Make sure input is an int, and don't over range
+    until [[ $index =~ $re_num ]] && [ 0 -le $index ] && [ $index -le $1 ]; do
+        read -p "Input index of your $2 interfaces name: " index
+    done
+
+    return $index
+}
+
+getIndex ${interfaces_length} "wireless"
+wlan_interfaces_name=${interfaces[$?]}
+
+getIndex ${interfaces_length} "ethernet"
+eth_interfaces_name=${interfaces[$?]}
+
+# ===== Set AP name =====
+
+ssid=''
+
+until [[ ${#ssid} -gt 0 ]]; do
+    read -p "Input your wifi AP name: " ssid
+done
+
+# ===== Set password =====
+password=''
+
+until [[ ${#password} -ge 8 ]]; do
+    read -p "Input your wifi AP password (8 char at least): " password
+done
+
+# ====== Gen configure file =====
+sed "s/%wlan_interface%/${wlan_interfaces_name}/" dnsmasq.conf > dnsmasq.conf.true
+sed -e "s/%wlan_interface%/${wlan_interfaces_name}/" -e "s/%password%/${password}/" -e "s/%ssid%/${ssid}/" hostapd.conf > hostapd.conf.true
 
 # Turn down servces
 service dnsmasq stop
@@ -21,11 +81,11 @@ rfkill unblock wlan
 killall hostapd
 killall dnsmasq
 
-# Open wlan0
-ifconfig wlan0 up
+# Open wlan
+ifconfig $wlan_interfaces_name up
 
-# Set wlan0 ip address
-ifconfig wlan0 192.168.43.1
+# Set wlan ip address
+ifconfig $wlan_interfaces_name 192.168.43.1
 
 # Configure NAT
 
@@ -33,18 +93,17 @@ ifconfig wlan0 192.168.43.1
 iptables -t nat -F
 
 # Add NAT rule
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $eth_interfaces_name -j MASQUERADE
 
 # Enable ip forwoad
 sysctl net.ipv4.ip_forward=1
-
 
 # ========= Open WIFI ========
 
 echo -e "\n===== Open WiFi... =====\n"
 
-dnsmasq -C ${BASEDIR}/dnsmasq.conf
-hostapd ${BASEDIR}/hostapd.conf
+dnsmasq -C dnsmasq.conf.true
+hostapd hostapd.conf.true
 
 echo -e "\nWiFi Stop, Cleaning......\n"
 
@@ -63,7 +122,10 @@ nmcli r wifi on
 killall hostapd
 killall dnsmasq
 
+# Delete temp configure files
+rm dnsmasq.conf.true
+rm hostapd.conf.true
+
 printf "\nDone!\n"
 
 read -n 1 -p "Press Enter key to exit..."
-
